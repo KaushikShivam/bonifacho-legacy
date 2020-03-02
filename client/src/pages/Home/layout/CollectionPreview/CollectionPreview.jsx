aaa480e1a2c7f3f154030eae7e8d31c560d1db8b
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 18:16:20 +0530

    Add CollectionPreview

---
+++ client/src/pages/Home/layout/CollectionPreview/CollectionPreview.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './CollectionPreview.scss';
+
+import Button from '../../components/Button/Button';
+import CustomLink from '../../components/CustomLink/CustomLink';
+import ArtworkSmall from '../../components/ArtworkSmall/ArtworkSmall';
+
+class CollectionPreview extends Component {
+  render() {
+    return (
+      <div className="CollectionPreview">
+        <div className="CollectionPreview-header">
+          <h3 className="CollectionPreview-title">BEST SELLERS</h3>
+          <div className="CollectionPreview-buttons">
+            <Button
+              backgroundColor="#AB9B8A"
+              padding="1.1rem 2rem"
+              fontSize="1.2rem"
+            >
+              LIMITED EDITIONS
+            </Button>
+            <Button
+              backgroundColor="black"
+              padding="1.1rem 2rem"
+              fontSize="1.2rem"
+            >
+              ORIGINAL ARTWORKS
+            </Button>
+          </div>
+        </div>
+        <div className="CollectionPreview-artworks">
+          <ArtworkSmall />
+          <ArtworkSmall />
+          <ArtworkSmall />
+          <ArtworkSmall />
+        </div>
+        <div className="ArtworkWeek-link">
+          <CustomLink>BROWSE ALL BEST-SELLERS IN ORIGINAL EDITIONS</CustomLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CollectionPreview;
