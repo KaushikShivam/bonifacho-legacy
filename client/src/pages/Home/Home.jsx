2093c217520d3856f1e3280e4788bdfb32a86cd1
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 14:40:23 +0530

    Style ArtworkWeek component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Layout
 import Hero from './layout/Hero/Hero';
+import ArtworkWeek from './layout/ArtworkWeek/ArtworkWeek';
 
 const Home = () => {
   return (
     <div className="Home">
       <Hero />
+      <ArtworkWeek />
     </div>
   );
 };
--- client/src/pages/Home/layout/ArtworkWeek/ArtworkWeek.jsx
+++ client/src/pages/Home/layout/ArtworkWeek/ArtworkWeek.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './ArtworkWeek.scss';
 
-import ArtworkBig from '../../components/ArtworkBig/ArtworkBig';
-import CustomLink from '../../components/CustomLink/CustomLink';
+// import ArtworkBig from './../../components/ArtworkBig/ArtworkBig';
+// import CustomLink from './../../components/CustomLink/CustomLink';
 
 const ArtworkWeek = () => {
   return (
     <div className="ArtworkWeek">
       <h2 className="ArtworkWeek-title">ARTWORKS OF THE WEEK</h2>
       <div className="ArtworkWeek-content">
-        <ArtworkBig />
-        <ArtworkBig />
+        {/* <ArtworkBig /> */}
+        {/* <ArtworkBig /> */}
       </div>
       <div className="ArtworkWeek-link">
-        <CustomLink>SHOP ALL ARTWORKS</CustomLink>
+        {/* <CustomLink>SHOP ALL ARTWORKS</CustomLink> */}
       </div>
     </div>
   );
