db21dd5ec0da4dbb26e9c31908648200ec9a167e
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 19:06:53 +0530

    Add FineArt component

---
+++ client/src/pages/Home/layout/FineArt/FineArt.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Fineart from '../../assets/Fine-art.png';
+import FormButton from '../../components/FormButton/FormButton';
+
+import './FineArt.scss';
+
+const FineArt = () => {
+  return (
+    <div className="FineArt">
+      <div className="FineArt-img">
+        <img src={Fineart} alt="Fine art" />
+      </div>
+      <div className="FineArt-content">
+        <h2>ORIGINAL FINE ART</h2>
+        <p>
+          House of 99 is not a standard gallery; we’re the first fine art house
+          in the world. We offer original, one-of-a-kind artworks that are
+          curated by our team to a museum-level standard from the well-known
+          contemporary artists of our time..
+        </p>
+        <FormButton>LEARN MORE</FormButton>
+      </div>
+    </div>
+  );
+};
+
+export default FineArt;
