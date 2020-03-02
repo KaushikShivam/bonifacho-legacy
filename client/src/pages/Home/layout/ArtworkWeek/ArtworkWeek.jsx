1af41cecdfc6fa7547f0d2c9b22d3c5ca4e022a8
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 14:39:04 +0530

    Create ArtworkWeek component

---
+++ client/src/pages/Home/layout/ArtworkWeek/ArtworkWeek.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './ArtworkWeek.scss';
+
+import ArtworkBig from '../../components/ArtworkBig/ArtworkBig';
+import CustomLink from '../../components/CustomLink/CustomLink';
+
+const ArtworkWeek = () => {
+  return (
+    <div className="ArtworkWeek">
+      <h2 className="ArtworkWeek-title">ARTWORKS OF THE WEEK</h2>
+      <div className="ArtworkWeek-content">
+        <ArtworkBig />
+        <ArtworkBig />
+      </div>
+      <div className="ArtworkWeek-link">
+        <CustomLink>SHOP ALL ARTWORKS</CustomLink>
+      </div>
+    </div>
+  );
+};
+
+export default ArtworkWeek;
