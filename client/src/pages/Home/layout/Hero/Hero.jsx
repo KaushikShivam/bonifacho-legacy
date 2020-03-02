091bcaa4367ba18e278fb585dd0d45c4b349a162
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 14:16:48 +0530

    Refactor Hero into a functional component

--- client/src/pages/Home/layout/Hero/Hero.jsx
+++ client/src/pages/Home/layout/Hero/Hero.jsx
@@ -2,31 +2,29 @@ import React, { Component } from 'react';
 import './Hero.scss';
 import heroBg from './../../../../assets/heroBg.png';
 
-class Hero extends Component {
-  render() {
-    return (
-      <div className="Hero" style={{ background: `url(${heroBg})` }}>
-        <h3 className="Hero-subtitle">HOUSE OF 99 PRESENTS</h3>
-        <h1 className="Hero-title">BONIFACHO</h1>
-        <div className="Hero-btns">
-          <button
-            backgroundColor="#AB9B8A"
-            padding="1.5rem 3rem"
-            fontSize="1.5rem"
-          >
-            SHOW ALL LIMITED EDITIONS
-          </button>
-          <button
-            backgroundColor="#000000"
-            padding="1.5rem 3rem"
-            fontSize="1.5rem"
-          >
-            SHOW ALL ORIGINAL ARTWORKS
-          </button>
-        </div>
+const Hero = () => {
+  return (
+    <div className="Hero" style={{ background: `url(${heroBg})` }}>
+      <h3 className="Hero-subtitle">HOUSE OF 99 PRESENTS</h3>
+      <h1 className="Hero-title">BONIFACHO</h1>
+      <div className="Hero-btns">
+        <button
+          backgroundColor="#AB9B8A"
+          padding="1.5rem 3rem"
+          fontSize="1.5rem"
+        >
+          SHOW ALL LIMITED EDITIONS
+        </button>
+        <button
+          backgroundColor="#000000"
+          padding="1.5rem 3rem"
+          fontSize="1.5rem"
+        >
+          SHOW ALL ORIGINAL ARTWORKS
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Hero;
