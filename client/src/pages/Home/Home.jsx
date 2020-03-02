25e95f0e1b456dffb615c9286f57721377179b60
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 14:16:12 +0530

    Create Hero component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+// Layout
+import Hero from './layout/Hero/Hero';
+
 const Home = () => {
-  return <div className="Home"></div>;
+  return (
+    <div className="Home">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
