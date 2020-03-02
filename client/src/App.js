b6e2e27467d6117479fa591ba9e2c43619aa724a
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-02 13:19:59 +0530

    Add Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './layout/Navbar/Navbar';
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
