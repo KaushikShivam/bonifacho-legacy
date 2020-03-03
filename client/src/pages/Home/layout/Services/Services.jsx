67cc6ec29575417cd1d1f963e0871b6b3203db10
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-03 19:00:30 +0530

    Create Services component

---
+++ client/src/pages/Home/layout/Services/Services.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Services.scss';
+
+import Service from '../../components/Service/Service';
+import cartIcon from '../../assets/cartIcon.png';
+import paymentIcon from '../../assets/paymentIcon.png';
+import phoneIcon from '../../assets/phoneIcon.png';
+
+const Services = () => {
+  return (
+    <div className="Services">
+      {/* <Service
+        icon={phoneIcon}
+        title="Customer Service"
+        description="+91-9711112922"
+      />
+      <Service
+        icon={cartIcon}
+        title="Free returns"
+        description="30 days return policy + free return pickup"
+      />
+      <Service
+        icon={paymentIcon}
+        title="Secure Payment"
+        description="Rest assurared about paying online"
+      /> */}
+    </div>
+  );
+};
+
+export default Services;
