fe0f3796340e865a540859c75b8fc1127d929b8c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-08 21:58:54 +0530

    Connect mongoose with express

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const dotenv = require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './config.env' });
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connected'));
+
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`);
 });
