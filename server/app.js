0dffe159583121f6a62852eaed4f09a1bc424645
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-08 22:04:37 +0530

    Add a simple artwork router and mount on express app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// import routers
+const artworkRouter = require('./routes/artworkRoutes');
+
 const app = express();
 
+// middlewares
+// Logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// mounting the routes
+app.use('/api/v1/artworks', artworkRouter);
 
 module.exports = app;
