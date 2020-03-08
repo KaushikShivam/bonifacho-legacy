1889d838c8ee78f0c8dcb9bde76752a6ed133195
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-08 22:21:21 +0530

    Add getall artwork to router

--- server/routes/artworkRoutes.js
+++ server/routes/artworkRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const artworkController = require('./../controllers/artworkController');
 
 const router = express.Router();
+
+router.route('/').get(artworkController.getAllArtworks);
 
 module.exports = router;
