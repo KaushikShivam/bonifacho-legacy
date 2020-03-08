8c79ef7823a64bfc3778f1121e74c2adab666cac
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-08 21:46:45 +0530

    Add basic express server

---
+++ server.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv').config({ path: './config.env' });
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
