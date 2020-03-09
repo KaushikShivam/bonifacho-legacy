29615f3cd91959d2a33578c4df0a084a4921992f
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-09 10:58:10 +0530

    Handle unhandled routes

--- server/app.js
+++ server/app.js
@@ -18,4 +18,11 @@ app.use(express.json({ limit: '10kb' }));
 // mounting the routes
 app.use('/api/v1/artworks', artworkRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 module.exports = app;
