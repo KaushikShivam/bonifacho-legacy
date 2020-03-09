60d32f0c9bddd630412e0728de13428488325f5a
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-09 14:40:56 +0530

    Add password and confirm password custom validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -20,7 +20,13 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(val) {
+        return val === this.password;
+      },
+      message: 'Password and password confirm does not match'
+    }
   }
 });
 
