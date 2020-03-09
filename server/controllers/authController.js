516f057185178cc5bc32b5852db2efefd18e4dc2
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-09 17:56:39 +0530

    Finish User auth functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -134,3 +134,44 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1. Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+  // 2. Set the new password if token has not expired and there is a user
+  if (!user) {
+    return next(new AppError('Toker is invalid or expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3. Update the changedPasswordAt for the current user
+  // 4. Log user in (send JWT)
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1. Get user from the DB
+  const user = await User.findById(req.user.id).select('+password');
+  // 2. Check if posted password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+  // 3. If so, update the password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // 4. Log the user in
+  createSendToken(user, 200, res);
+});
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,17 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.signup);
-router.route('/login').post(authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+// router.get('/me', userController.getMe, userController.getUser);
+// router.patch('/updateMe', userController.updateMe);
+// router.delete('/deleteMe', userController.deleteMe);
 
 module.exports = router;
