f4a786b9715623eda5079d559e56c3f31345e4b8
shivam <shivam.kaushik@outlook.com>
2020-04-03 23:11:46 +0530

    Finish creating artwork form and styles

--- client/src/components/ArtworkForm/ArtworkForm.jsx
+++ client/src/components/ArtworkForm/ArtworkForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
+import './ArtworkForm.scss';
+
 import FormInput from './../FormInput/FormInput';
-// import CustomButton from './../CustomButton';
+import FormButton from './../FormButton/FormButton';
 
 const ArtworkForm = () => {
   const [formData, setFormData] = useState({
@@ -18,48 +20,53 @@ const ArtworkForm = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   return (
-    <form>
-      <FormInput
-        name="name"
-        type="text"
-        value={name}
-        handleChange={handleChange}
-        label="Name"
-        required
-      />
-      <FormInput
-        name="image"
-        type="text"
-        value={image}
-        handleChange={handleChange}
-        label="Image"
-        required
-      />
-      <FormInput
-        name="edition"
-        type="text"
-        value={edition}
-        handleChange={handleChange}
-        label="Edition"
-        required
-      />
-      <FormInput
-        name="price"
-        type="number"
-        value={price}
-        handleChange={handleChange}
-        label="Price"
-        required
-      />
-      <FormInput
-        name="category"
-        type="text"
-        value={category}
-        handleChange={handleChange}
-        label="Category"
-        required
-      />
-    </form>
+    <div className="ArtworkForm">
+      <h2 className="ArtworkForm__title">Create Your Artwork</h2>
+      <span className="ArtworkForm__subtitle">All fileds are mandatory</span>
+      <form>
+        <FormInput
+          name="name"
+          type="text"
+          value={name}
+          handleChange={handleChange}
+          label="Name"
+          required
+        />
+        <FormInput
+          name="image"
+          type="text"
+          value={image}
+          handleChange={handleChange}
+          label="Image"
+          required
+        />
+        <FormInput
+          name="edition"
+          type="text"
+          value={edition}
+          handleChange={handleChange}
+          label="Edition"
+          required
+        />
+        <FormInput
+          name="price"
+          type="number"
+          value={price}
+          handleChange={handleChange}
+          label="Price"
+          required
+        />
+        <FormInput
+          name="category"
+          type="text"
+          value={category}
+          handleChange={handleChange}
+          label="Category"
+          required
+        />
+        <FormButton type="submit">Create</FormButton>
+      </form>
+    </div>
   );
 };
 
