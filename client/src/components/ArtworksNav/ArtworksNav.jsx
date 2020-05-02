78c62807cee4ed61b40c122c8dcbbe884cf9dcba
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-02 17:37:07 +0530

    Finish slider styles

--- client/src/components/ArtworksNav/ArtworksNav.jsx
+++ client/src/components/ArtworksNav/ArtworksNav.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Slider from 'rc-slider';
 
+import 'rc-slider/assets/index.css';
 import './ArtworksNav.scss';
 
 import CheckBox from './../CheckBox/CheckBox';
@@ -24,6 +26,19 @@ const ArtworksNav = () => {
 					<CheckBox>Impressionist</CheckBox>
 					<CheckBox>Photography</CheckBox>
 					<CheckBox>Abstract</CheckBox>
+				</div>
+			</section>
+
+			<section className='ArtworksNav__section'>
+				<h4 className='ArtworksNav__header'>Price</h4>
+				<div className='ArtworksNav__slider'>
+					<Slider
+						defaultValue={0}
+						min={100}
+						max={900}
+						step={100}
+						// onChange={changeLevel}
+					/>
 				</div>
 			</section>
 		</main>
