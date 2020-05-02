beabc0c941b684a0efc75e97159a3205a5c330e3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-05-02 15:05:32 +0530

    Remove logs

--- client/src/pages/Home/layout/ArtworkWeek/ArtworkWeek.jsx
+++ client/src/pages/Home/layout/ArtworkWeek/ArtworkWeek.jsx
@@ -1,40 +1,36 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import './ArtworkWeek.scss';
 
 import ArtworkBig from './../../../../components/ArtworkBig/ArtworkBig';
 import CustomLink from './../../../../components/CustomLink/CustomLink';
 
-import { getWeeklyArtworks } from './../../../../redux/actions/artwork'
+import { getWeeklyArtworks } from './../../../../redux/actions/artwork';
 
 const ArtworkWeek = ({ artworkWeek, getWeeklyArtworks }) => {
+	useEffect(() => {
+		getWeeklyArtworks();
+	}, [getWeeklyArtworks]);
 
-  useEffect(() => {
-    getWeeklyArtworks()
-  }, [getWeeklyArtworks])
-
-  console.log(artworkWeek)
-
-  return (
-    <div className="ArtworkWeek">
-      <h2 className="ArtworkWeek-title">ARTWORKS OF THE WEEK</h2>
-      <div className="ArtworkWeek-content">
-        {
-          artworkWeek.map(artwork => <ArtworkBig key={artwork.id} {...artwork} />)
-        }
-
-      </div>
-      <div className="ArtworkWeek-link">
-        <CustomLink>SHOP ALL ARTWORKS</CustomLink>
-      </div>
-    </div>
-  );
+	return (
+		<div className='ArtworkWeek'>
+			<h2 className='ArtworkWeek-title'>ARTWORKS OF THE WEEK</h2>
+			<div className='ArtworkWeek-content'>
+				{artworkWeek.map((artwork) => (
+					<ArtworkBig key={artwork.id} {...artwork} />
+				))}
+			</div>
+			<div className='ArtworkWeek-link'>
+				<CustomLink>SHOP ALL ARTWORKS</CustomLink>
+			</div>
+		</div>
+	);
 };
 
-const mapStateToProps = state => ({ artworkWeek: state.artwork.artworkWeek })
+const mapStateToProps = (state) => ({ artworkWeek: state.artwork.artworkWeek });
 
-const mapdispatchToProps = dispatch => ({
-  getWeeklyArtworks: () => dispatch(getWeeklyArtworks())
-})
+const mapdispatchToProps = (dispatch) => ({
+	getWeeklyArtworks: () => dispatch(getWeeklyArtworks()),
+});
 
 export default connect(mapStateToProps, mapdispatchToProps)(ArtworkWeek);
--- server/controllers/artworkController.js
+++ server/controllers/artworkController.js
@@ -5,123 +5,119 @@ const AppError = require('./../utils/AppError');
 const APIFeatures = require('./../utils/APIFeatures');
 
 exports.getAllArtworks = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Artwork.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+	const features = new APIFeatures(Artwork.find(), req.query)
+		.filter()
+		.sort()
+		.limitFields()
+		.paginate();
 
-  //Execute query
-  const artworks = await features.query;
+	//Execute query
+	const artworks = await features.query;
 
-  res.status(200).json({
-    status: 'success',
-    results: artworks.length,
-    data: {
-      artworks,
-    },
-  });
+	res.status(200).json({
+		status: 'success',
+		results: artworks.length,
+		data: {
+			artworks,
+		},
+	});
 });
 
 exports.getArtworkWithCategories = catchAsync(async (req, res, next) => {
-  const artworks = await Artwork.aggregate([
-    {
-      $group: {
-        _id: '$category',
-        artworks: { $push: '$$ROOT' },
-      },
-    },
-  ]);
+	const artworks = await Artwork.aggregate([
+		{
+			$group: {
+				_id: '$category',
+				artworks: { $push: '$$ROOT' },
+			},
+		},
+	]);
 
-  res.status(200).json({
-    data: {
-      artworks,
-    },
-  });
+	res.status(200).json({
+		data: {
+			artworks,
+		},
+	});
 });
 
 exports.getArtworkWeek = catchAsync(async (req, res, next) => {
-  const artworks = await Artwork.aggregate([{ $sample: { size: 2 } }]);
-  // const artworks = await Artwork.find().limit(2);
+	const artworks = await Artwork.aggregate([{ $sample: { size: 2 } }]);
+	// const artworks = await Artwork.find().limit(2);
 
-  console.log(artworks)
-
-  res.status(200).json({
-    data: {
-      artworks,
-    },
-  });
+	res.status(200).json({
+		data: {
+			artworks,
+		},
+	});
 });
 
 exports.getMyArtworks = (req, res, next) => {
-  req.query.artist = req.user.id;
-  next();
+	req.query.artist = req.user.id;
+	next();
 };
 
 exports.createArtwork = catchAsync(async (req, res, next) => {
-  const reqBody = { ...req.body, artist: req.user.id };
+	const reqBody = { ...req.body, artist: req.user.id };
 
-  const artwork = await Artwork.create(reqBody);
+	const artwork = await Artwork.create(reqBody);
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      artwork,
-    },
-  });
+	res.status(201).json({
+		status: 'success',
+		data: {
+			artwork,
+		},
+	});
 });
 
 exports.getArtwork = catchAsync(async (req, res, next) => {
-  const artwork = await Artwork.findOne({
-    _id: req.params.id,
-    artist: req.user.id,
-  });
+	const artwork = await Artwork.findOne({
+		_id: req.params.id,
+		artist: req.user.id,
+	});
 
-  if (!artwork) {
-    return next(new AppError('No Artwork found with this ID', 404));
-  }
+	if (!artwork) {
+		return next(new AppError('No Artwork found with this ID', 404));
+	}
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      artwork,
-    },
-  });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			artwork,
+		},
+	});
 });
 
 exports.updateArtwork = catchAsync(async (req, res, next) => {
-  const artwork = await Artwork.findOneAndUpdate(
-    { _id: req.params.id, artist: req.user.id },
-    req.body,
-    { new: true, runValidators: true }
-  );
-  console.log(req.body);
-  console.log(artwork);
+	const artwork = await Artwork.findOneAndUpdate(
+		{ _id: req.params.id, artist: req.user.id },
+		req.body,
+		{ new: true, runValidators: true }
+	);
 
-  if (!artwork) {
-    return next(new AppError('No Artwork found with this ID', 404));
-  }
+	if (!artwork) {
+		return next(new AppError('No Artwork found with this ID', 404));
+	}
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      artwork,
-    },
-  });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			artwork,
+		},
+	});
 });
 
 exports.deleteArtwork = catchAsync(async (req, res, next) => {
-  const artwork = await Artwork.findOneAndRemove({
-    _id: req.params.id,
-    artist: req.user.id,
-  });
+	const artwork = await Artwork.findOneAndRemove({
+		_id: req.params.id,
+		artist: req.user.id,
+	});
 
-  if (!artwork) {
-    return next(new AppError('No Artwork found with this ID', 404));
-  }
+	if (!artwork) {
+		return next(new AppError('No Artwork found with this ID', 404));
+	}
 
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+	res.status(204).json({
+		status: 'success',
+		data: null,
+	});
 });
